d6e0a2b69ee2e0b05a94abfaabf0a95662a75179
GoodPhD <yanghb1993@gmail.com>
2020-06-19 12:06:46 -0500

    two methods of data exchange
    XMLHttpRequest and jsonp

--- homepage.js
+++ homepage.js
@@ -1,10 +1,10 @@
 
 
-var map = null;
+// var map = null;
 window.onload = init;
 
 function init() {
-	getLocation();
+	// getLocation();
 	var button = document.getElementById("addButton");
 	button.onclick = handleButtonClick;
 }
@@ -23,37 +23,49 @@ function handleButtonClick() {
 	}	
 }
 
-function getLocation() {
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(displayLocation);
+
+function updateSales(sales) {
+	var salesDiv = document.getElementById("sales");
+	for (var i = sales.length - 1; i >= 0; i--) {
+		var sale = sales[i];
+		var div = document.createElement("div");
+		div .setAttribute("class", "saleItem");
+		div.innerHTML = sale.name + " sold " + sale.sales + " gumballs";
+		salesDiv.appendChild(div);
 	}
-	else {
-		alert("No geolocation support!");
-	}
 }
 
-function displayLocation (position) {
-	var latitude = position.coords.latitude;
-	var longitude = position.coords.longitude;
+// function getLocation() {
+// 	if (navigator.geolocation) {
+// 		navigator.geolocation.getCurrentPosition(displayLocation);
+// 	}
+// 	else {
+// 		alert("No geolocation support!");
+// 	}
+// }
 
-	var div = document.getElementById("location");
-	div.innerHTML = "You are at Latitude: " + latitude + " , Longitude: " + longitude 
-					+ " with accuracy: " + position.coords.accuracy + " meters accuracy";
+// function displayLocation (position) {
+// 	var latitude = position.coords.latitude;
+// 	var longitude = position.coords.longitude;
 
-	showMap(position.coords);
-}
+// 	var div = document.getElementById("location");
+// 	div.innerHTML = "You are at Latitude: " + latitude + " , Longitude: " + longitude 
+// 					+ " with accuracy: " + position.coords.accuracy + " meters accuracy";
 
-function showMap(coords) {
-	var googleLatAndLong = new google.maps.LatLng(coords.latitude, 
-												  coords.longitude);
-	var mapOptions = {
-		zoom: 10,
-		center: googleLatAndLong,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	var mapDiv = document.getElementById("map");
-	map = new google.maps.Map(mapDiv, mapOptions);
-}
+// 	// showMap(position.coords);
+// }
+
+// function showMap(coords) {
+// 	var googleLatAndLong = new google.maps.LatLng(coords.latitude, 
+// 												  coords.longitude);
+// 	var mapOptions = {
+// 		zoom: 10,
+// 		center: googleLatAndLong,
+// 		mapTypeId: google.maps.MapTypeId.ROADMAP
+// 	};
+// 	var mapDiv = document.getElementById("map");
+// 	map = new google.maps.Map(mapDiv, mapOptions);
+// }
 
 
 
